e1a3f8b6d48b39974d2eed364346b17fb1da5d4f
dzyrick2 <daniel.gayao7@gmail.com>
2023-03-24 22:34:56 +0800

    setup the file structure

---
+++ src/components/Homehero.tsx
@@ -0,0 +1,9 @@
+import "../components/sass/Homehero.scss"
+
+export default function Homehero() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import "../components/sass/Navbar.scss"
+
+export default function Navbar() {
+    return (
+        <header className='navbar-container'>
+            <div className="logo">
+
+            </div>
+            <div className="menu-item">
+
+            </div>
+        </header>
+    )
+}
