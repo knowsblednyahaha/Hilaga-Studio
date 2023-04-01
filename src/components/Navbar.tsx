fe2a260856d9cef7f5bdc36c6a1723286d1c4fe6
dzyrick2 <daniel.gayao7@gmail.com>
2023-04-01 23:07:40 +0800

    added item in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,14 @@ export default function Navbar() {
     return (
         <header className='navbar-container'>
             <div className="logo">
-                <img src="/logo.png" alt="" />
+                <img src="/logo.png" alt="logo" />
             </div>
             <div className="menu-item">
+                <ul>
+                    <li>About</li>
+                    <li>Works</li>
+                    <li>Contact</li>
+                </ul>
                 <span>HIRE US</span>
             </div>
         </header>
