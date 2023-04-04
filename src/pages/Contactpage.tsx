f1b8593a6b3a9aaf3971ce1131ac6f2c2280ad14
dzyrick2 <daniel.gayao7@gmail.com>
2023-04-04 22:47:06 +0800

    added setup for everypages

---
+++ src/pages/Contactpage.tsx
@@ -0,0 +1,5 @@
+export default function Contactpage() {
+    return (
+        <div>Aboutpage</div>
+    )
+}
---
+++ src/pages/Workpage.tsx
@@ -0,0 +1,5 @@
+export default function Workpage() {
+    return (
+        <div>Aboutpage</div>
+    )
+}
