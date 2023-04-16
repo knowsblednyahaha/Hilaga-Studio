4923148560b8d45af3ae9c39e01d738380f954a1
dzyrick2 <daniel.gayao7@gmail.com>
2023-04-16 23:23:11 +0800

    setup the horizontal scrolling

--- src/components/AboutComponents/AboutHero.tsx
+++ src/components/AboutComponents/AboutHero.tsx
@@ -1,21 +1,26 @@
 import "../sass/Abouthero.scss"
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 export default function AboutHero() {
   return (
-    <div className='about-hero-container'>
-      <div className="text-header">
-          <h1>Hilaga About</h1>
-      </div>
-      <div className="text-info-1">
-          <p>Lorem Ipsum is simply dummy text of the 
-            printing and typesetting industry. Lorem 
-            Ipsum has been the industry's standard 
-            dummy text ever since the 1500s, when an 
-            unknown printer took a galley of type and 
-            scrambled </p>
-      </div>
-      <div className="img-1">
-        <img src="/1.jpg" alt="image-1" />
+    <div className="container">
+      <div className='about-hero-container panel'>
+        <div className="text-header">
+            <h1>Hilaga About</h1>
+        </div>
+        <div className="text-info-1">
+            <p>Lorem Ipsum is simply dummy text of the 
+              printing and typesetting industry. Lorem 
+              Ipsum has been the industry's standard 
+              dummy text ever since the 1500s, when an 
+              unknown printer took a galley of type and 
+              scrambled </p>
+        </div>
+        <div className="img-1">
+          <img src="/1.jpg" alt="image-1" />
+        </div>
       </div>
     </div>
   )
