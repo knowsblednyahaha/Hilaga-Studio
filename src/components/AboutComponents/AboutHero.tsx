5ff6863a560180e0f05e383a045108c3bd53963f
dzyrick2 <daniel.gayao7@gmail.com>
2023-04-19 23:53:48 +0800

    fixed horizontal scroll

--- src/components/AboutComponents/AboutHero.tsx
+++ src/components/AboutComponents/AboutHero.tsx
@@ -14,46 +14,48 @@ export default function AboutHero() {
     ease: "none",
     scrollTrigger: {
       trigger: ".container",
-      pin: true,
+      pin: ".wrapper",
       scrub: 1,
-      snap: 1 / (sections.length - 1),
+      // snap: 1 / (sections.length - 1),
       end: () => "+=3000"
     }
   });
 
 
   return (
-    <div className="container">
-      <div className='about-hero-container panel'>
-        <div className="text-header">
-            <h1>Hilaga About</h1>
+    <div className="wrapper">
+      <div className="container">
+        <div className='about-hero-container panel'>
+          <div className="text-header">
+              <h1>Hilaga About</h1>
+          </div>
+          <div className="text-info-1">
+              <p>Lorem Ipsum is simply dummy text of the 
+                printing and typesetting industry. Lorem 
+                Ipsum has been the industry's standard 
+                dummy text ever since the 1500s, when an 
+                unknown printer took a galley of type and 
+                scrambled </p>
+          </div>
+          <div className="img-1">
+            <img src="/1.jpg" alt="image-1" />
+          </div>
         </div>
-        <div className="text-info-1">
-            <p>Lorem Ipsum is simply dummy text of the 
-              printing and typesetting industry. Lorem 
-              Ipsum has been the industry's standard 
-              dummy text ever since the 1500s, when an 
-              unknown printer took a galley of type and 
-              scrambled </p>
-        </div>
-        <div className="img-1">
-          <img src="/1.jpg" alt="image-1" />
-        </div>
-      </div>
-      <div className='about-hero-container panel'>
-        <div className="text-header">
-            <h1>Hilaga About</h1>
-        </div>
-        <div className="text-info-1">
-            <p>Lorem Ipsum is simply dummy text of the 
-              printing and typesetting industry. Lorem 
-              Ipsum has been the industry's standard 
-              dummy text ever since the 1500s, when an 
-              unknown printer took a galley of type and 
-              scrambled </p>
-        </div>
-        <div className="img-1">
-          <img src="/1.jpg" alt="image-1" />
+        <div className=' panel'>
+          <div className="text-header">
+              <h1>Hilaga About</h1>
+          </div>
+          <div className="text-info-1">
+              <p>Lorem Ipsum is simply dummy text of the 
+                printing and typesetting industry. Lorem 
+                Ipsum has been the industry's standard 
+                dummy text ever since the 1500s, when an 
+                unknown printer took a galley of type and 
+                scrambled </p>
+          </div>
+          <div className="img-1">
+            <img src="/1.jpg" alt="image-1" />
+          </div>
         </div>
       </div>
     </div>
