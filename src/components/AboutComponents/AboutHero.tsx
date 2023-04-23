f548633f49afe887b21932d8b539b6c2e636d272
dzyrick2 <daniel.gayao7@gmail.com>
2023-04-23 23:40:20 +0800

    removed the horizontal scroll

--- src/components/AboutComponents/AboutHero.tsx
+++ src/components/AboutComponents/AboutHero.tsx
@@ -2,48 +2,42 @@ import "../sass/Abouthero.scss"
 
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useEffect, useLayoutEffect, useRef } from "react";
+import { useLayoutEffect, useRef } from "react";
 gsap.registerPlugin(ScrollTrigger);
 
 
 export default function AboutHero() {
-  const component = useRef(null);
-  const slider = useRef(null);
+  // const component = useRef(null);
+  // const slider = useRef(null);
 
-  useLayoutEffect(() => {
-    let ctx = gsap.context(() => {
-      let panels = gsap.utils.toArray(".panel");
-      gsap.to(panels, {
-        xPercent: -100 * (panels.length - 1),
-        ease: "none",
-        scrollTrigger: {
-          trigger: slider.current,
-          pin: true,
-          scrub: 1,
-          snap: 1 / (panels.length - 1),
-          end: () => "+=" + window.innerWidth,
-          // markers: true
-        }
-      });
-    }, component);
-    return () => ctx.revert();
-  });
+  // useLayoutEffect(() => {
+  //   let ctx = gsap.context(() => {
+  //     const pixelsPause = 300;
+  //     let panels = gsap.utils.toArray(".panel");
+  //     gsap.to(panels, {
+  //       xPercent: -100 * (panels.length - 1),
+  //       ease: "none",
+  //       scrollTrigger: {
+  //         trigger: slider.current,
+  //         scrub: 1,
+  //         snap: 1 / (panels.length - 1),
+  //         start: `top+=${pixelsPause} top`,
+  //         end: () => "+=" + window.innerWidth * panels.length,
+  //         markers: { startColor: "fuchsia", endColor: "fuchsia", indent: 200 },
+  //       },
+  //     });
+  //     ScrollTrigger.create({
+  //       trigger: slider.current,
+  //       end: () => "+=" + (window.innerWidth * panels.length + pixelsPause),
+  //       markers: true,
+  //       pin: true,
+  //     });
+  //   }, component);
+  //   return () => ctx.revert();
+  // });
   return (
     <div className="wrapper" ref={component}>
-      <div ref={slider} className="container">
-        <div className="description panel blue">
-          <div>
-            SCROLL DOWN
-            <div className="scroll-down">
-              <div className="arrow"></div>
-            </div>
-          </div>
-        </div>
-        <div className="panel red">ONE</div>
-        <div className="panel orange">TWO</div>
-        <div className="panel purple">THREE</div>
-      </div>
-      <div className="lastContainer">Last Container</div>
+      
     </div>
   )
 }
