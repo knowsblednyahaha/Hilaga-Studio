c3666e2bb3f9f711b783222c4951be5697caab9a
dzyrick2 <daniel.gayao7@gmail.com>
2023-04-29 23:37:21 +0800

    removed locomotive scroll

--- src/components/AboutComponents/AboutHero.tsx
+++ src/components/AboutComponents/AboutHero.tsx
@@ -2,41 +2,14 @@ import "../sass/Abouthero.scss"
 
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useLayoutEffect, useRef } from "react";
-import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
-gsap.registerPlugin(ScrollTrigger);
+import { useLayoutEffect, useRef, useEffect} from "react";
 
 
 export default function AboutHero() {
-  const containerRef = useRef(null)
-  return (
-    <LocomotiveScrollProvider
-  options={
-    {
-      smooth: true,
-      // ... all available Locomotive Scroll instance options 
-    }
-  }
-  watch={
-    [
-      //..all the dependencies you want to watch to update the scroll.
-      //  Basicaly, you would want to watch page/location changes
-      //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
-    ]
-  }
-  containerRef={containerRef}
->
-  <main data-scroll-container ref={containerRef}>
-  <div className="main-container">
-      <div className="scroll-container">
-        <div className="content">
-          <div className="about-container">
 
-          </div>
-        </div>
-      </div>
-    </div>
-  </main>
-</LocomotiveScrollProvider>
+  return (
+    <>
+    
+    </>
   )
 }
