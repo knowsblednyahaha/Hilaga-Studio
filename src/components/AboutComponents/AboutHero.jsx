a6a3a0e4e6ce31f0bd3d06ad56a34c9581206c39
dzyrick2 <daniel.gayao7@gmail.com>
2023-04-30 23:48:01 +0800

    added horizontal scroll

--- src/components/AboutComponents/AboutHero.jsx
+++ src/components/AboutComponents/AboutHero.jsx
@@ -6,8 +6,29 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 export default function AboutHero() {
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const scrollContainer = scrollContainerRef.current;
+
+    if (scrollContainer) {
+      // Disable native scrolling on the container
+      scrollContainer.style.overflowX = 'hidden';
+
+      // Calculate the width of the scrollable content
+      const scrollWidth = scrollContainer.scrollWidth - scrollContainer.clientWidth;
+
+      // Set up the GSAP animation
+      const timeline = gsap.timeline({ repeat: -1, paused: true });
+      timeline.to(scrollContainer, { duration: 10, x: -scrollWidth });
+
+      // Start the animation
+      timeline.play();
+    }
+  }, []);
   return (
-    <div className="main-container">
+    <div ref={scrollContainerRef}>
+      <div className="main-container">
+      </div>
     </div>
   )
 }
