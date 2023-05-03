2e31e445cd8f4a3f6aa7af1145d69d0f6f0f6117
dzyrick2 <daniel.gayao7@gmail.com>
2023-05-03 23:48:23 +0800

    attendance

--- src/components/AboutComponents/AboutHero.tsx
+++ src/components/AboutComponents/AboutHero.tsx
@@ -7,29 +7,11 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 export default function AboutHero() {
-  const scrollContainerRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    const scrollContainer = scrollContainerRef.current;
 
-    if (scrollContainer) {
-      // Disable native scrolling on the container
-      scrollContainer.style.overflowX = 'hidden';
-
-      // Calculate the width of the scrollable content
-      const scrollWidth = scrollContainer.scrollWidth - scrollContainer.clientWidth;
-
-      // Set up the GSAP animation
-      const timeline = gsap.timeline({ repeat: -1, paused: true });
-      timeline.to(scrollContainer, { duration: 10, x: -scrollWidth });
-
-      // Start the animation
-      timeline.play();
-    }
-  }, []);
   return (
-    <div ref={scrollContainerRef}>
       <div className="main-container">
+        <div className="red"></div>
+        <div className="blue"></div>
       </div>
-    </div>
   )
 }
